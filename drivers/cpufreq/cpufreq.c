cb4c32eef656f8ba5d6ef6548acc58db1db22067
faux123 <reioux@gmail.com>
2011-11-20 21:35:42 -0800

    cpufreq: fix cpu freq issues
    fixed frequency revert to power max freq when 2nd cpu is online

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -703,10 +703,16 @@ static ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf)
 
 static ssize_t store_UV_mV_table(struct cpufreq_policy *policy, const char *buf, size_t count)
 {
-	int ret = sscanf( buf, "%i %i %i %i %i %i %i", &FakeShmoo_UV_mV_Ptr[6], 
-								&FakeShmoo_UV_mV_Ptr[5], &FakeShmoo_UV_mV_Ptr[4], 
-								&FakeShmoo_UV_mV_Ptr[3], &FakeShmoo_UV_mV_Ptr[2], 
-								&FakeShmoo_UV_mV_Ptr[1], &FakeShmoo_UV_mV_Ptr[0] );
+
+	int ret = sscanf( buf, "%i %i %i %i %i %i %i", 
+			&FakeShmoo_UV_mV_Ptr[6],
+			&FakeShmoo_UV_mV_Ptr[5],
+			&FakeShmoo_UV_mV_Ptr[4],
+			&FakeShmoo_UV_mV_Ptr[3],
+			&FakeShmoo_UV_mV_Ptr[2],
+			&FakeShmoo_UV_mV_Ptr[1],
+			&FakeShmoo_UV_mV_Ptr[0] );
+
 	if (ret != 1)
 		return -EINVAL;
 
@@ -1088,8 +1094,8 @@ static int cpufreq_add_dev(struct sys_device *sys_dev)
 #ifdef CONFIG_HOTPLUG_CPU
 	for_each_online_cpu(sibling) {
 		cp = per_cpu(cpufreq_cpu_data, sibling);
-		if (cp && cp->governor &&
-		    (cpumask_test_cpu(cpu, cp->related_cpus))) {
+		dprintk("found sibling %d\n", sibling);
+		if (cp != NULL) {
 			dprintk("found sibling CPU, copying policy\n");
 			policy->governor = cp->governor;
 			policy->min = cp->min;
